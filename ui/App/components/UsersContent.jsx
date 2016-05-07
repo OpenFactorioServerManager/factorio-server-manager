8535bf59f6fd2433f83fddd41a5440b47ab6a207
majormjr <mitch@r00t.ca>
2016-05-07 17:19:09 -0400

    added form for adding users

--- ui/App/components/UsersContent.jsx
+++ ui/App/components/UsersContent.jsx
@@ -4,6 +4,8 @@ import {IndexLink} from 'react-router';
 class UsersContent extends React.Component {
     constructor(props) {
         super(props);
+        this.listUsers = this.listUsers.bind(this);
+        this.createUser = this.createUser.bind(this);
         this.state = {
             users: [],
         }
@@ -32,12 +34,19 @@ class UsersContent extends React.Component {
 
     createUser(e) {
         e.preventDefault();
+        console.log(this.refs);
         let user = {
-            username: this.refs.username,
-            role: this.refs.role,
-            password: this.refs.password,
-            email: this.refs.email,
+            username: this.refs.username.value,
+            // Add handler for listing roles
+            role: "admin",
+            password: this.refs.password.value,
+            email: this.refs.email.value,
         }
+        if (user.password !== this.refs.passwordConfirm.value) {
+            console.log("passwords do not match")
+            return
+        }
+
         $.ajax({
             type: "POST",
             url: "/api/user/add",
@@ -45,7 +54,8 @@ class UsersContent extends React.Component {
             data: JSON.stringify(user),
             success: (resp) => {
                 if (resp.success === true) {
-                    alert("User: ", user.username, " added successfully.")
+                    alert("User: " + user.username + " added successfully.");
+                    this.listUsers();
                 } else {
                     alert("Error deleting user: ", resp.data)
                 }
@@ -78,13 +88,35 @@ class UsersContent extends React.Component {
                         {this.state.users.map( (user, i) => {
                             return(
                                 <div>
-                                <h3>{user.Username}</h3>        
-                                <h3>{user.Role}</h3>        
-                                <h3>{user.Email}</h3>        
+                                <h4>{user.Username}</h4>        
+
+
                                 </div>
                             )                                    
                         })}
 
+                        <h4>Add user</h4>
+
+                        <form action="" onSubmit={this.createUser}>
+                            <div className="form-group">
+                                <label for="username">Username</label>
+                                <input ref="username" type="text" className="form-control" id="username" placeholder="Enter username" />
+                            </div> 
+                            <div className="form-group">
+                                <label for="password">Password</label>
+                                <input ref="password" type="text" className="form-control" id="password" placeholder="Enter password" />
+                            </div> 
+                            <div className="form-group">
+                                <label for="password">Password confirmation</label>
+                                <input ref="passwordConfirm" type="text" className="form-control" id="password" placeholder="Enter password again" />
+                            </div> 
+                            <div className="form-group">
+                                <label for="email">Email address</label>
+                                <input ref="email" type="text" className="form-control" id="email" placeholder="Enter email again" />
+                            </div> 
+                        
+                            <button className="btn btn-block btn-success" type="submit"><i className="fa fa-plus fa-fw"></i>Add User</button>
+                        </form>
                         </div>
                     </div>
                 </section>
