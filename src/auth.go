6c73277616c1678639be80c27b131e46cd82f90b
Kyle Bostelmann <kyle@bostelk.ca>
2016-09-29 18:35:12 -0300

    Add support for defining the cookie encryption key in the server config. Rename createInitialUser to CreateOrUpdateUser.

--- src/auth.go
+++ src/auth.go
@@ -23,7 +23,7 @@ func initAuth() *AuthHTTP {
 	return &AuthHTTP{}
 }
 
-func (auth *AuthHTTP) createAuthDb(backendFile string) error {
+func (auth *AuthHTTP) CreateAuth(backendFile string, cookieKey string) error {
 	var err error
 	os.Mkdir(backendFile, 0755)
 
@@ -33,22 +33,20 @@ func (auth *AuthHTTP) createAuthDb(backendFile string) error {
 		return err
 	}
 
+	roles := make(map[string]httpauth.Role)
+	roles["user"] = 30
+	roles["admin"] = 80
+
+	auth.aaa, err = httpauth.NewAuthorizer(auth.backend, []byte(cookieKey), "user", roles)
+	if err != nil {
+		log.Printf("Error creating authorizer: %s", err)
+		return err
+	}
+
 	return nil
 }
 
-func (auth *AuthHTTP) createRoles() {
-	var err error
-	roles := make(map[string]httpauth.Role)
-
-	roles["user"] = 30
-	roles["admin"] = 80
-	auth.aaa, err = httpauth.NewAuthorizer(auth.backend, []byte("topsecretkey"), "user", roles)
-	if err != nil {
-		log.Printf("Error creating roles: %s", err)
-	}
-}
-
-func (auth *AuthHTTP) createInitialUser(username, password, role, email string) error {
+func (auth *AuthHTTP) CreateOrUpdateUser(username, password, role, email string) error {
 	user := httpauth.UserData{Username: username, Role: role, Email: email}
 	err := auth.backend.SaveUser(user)
 	if err != nil {
@@ -58,11 +56,11 @@ func (auth *AuthHTTP) createInitialUser(username, password, role, email string) 
 
 	err = auth.aaa.Update(nil, nil, username, password, email)
 	if err != nil {
-		log.Printf("Error saving user: %s", err)
+		log.Printf("Error updating user: %s", err)
 		return err
 	}
 
-	log.Printf("Created initial user: %s", user.Username)
+	log.Printf("Created user: %s", user.Username)
 
 	return nil
 }
