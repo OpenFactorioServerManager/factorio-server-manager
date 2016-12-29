a30aec170f944e4380537c8e68d037f26293e1f9
majormjr <mitch@r00t.ca>
2016-12-29 08:59:50 -0500

    added check for running Factorio server before sending command

--- src/wsroutes.go
+++ src/wsroutes.go
@@ -22,17 +22,19 @@ func logSubscribe(client *Client, data interface{}) {
 }
 
 func commandSend(client *Client, data interface{}) {
-	go func() {
-		log.Printf("Received command: %v", data)
+	if FactorioServ.Running {
+		go func() {
+			log.Printf("Received command: %v", data)
 
-		req_id, err := FactorioServ.Rcon.Write(data.(string))
-		if err != nil {
-			log.Printf("Error sending rcon command: %s", err)
-			return
-		}
+			req_id, err := FactorioServ.Rcon.Write(data.(string))
+			if err != nil {
+				log.Printf("Error sending rcon command: %s", err)
+				return
+			}
 
-		log.Printf("Command send to Factorio: %s, with rcon request id: %v", data, req_id)
+			log.Printf("Command send to Factorio: %s, with rcon request id: %v", data, req_id)
 
-		client.send <- Message{"receive command", data}
-	}()
+			client.send <- Message{"receive command", data}
+		}()
+	}
 }
