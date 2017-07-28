98ba7bc9e5181d05c121e73c18b17d39c54838cb
knoxfighter <philipp@asdff45.de>
2017-07-28 18:45:16 +0200

    list of installed mods now built out of mod-list.json

--- src/mods.go
+++ src/mods.go
@@ -9,41 +9,33 @@ import (
 
 type Mod struct {
     Name    string `json:"name"`
-    Enabled bool   `json:"enabled,string"`
+    Enabled bool   `json:"enabled"`
+}
+
+type ModsList struct {
+    Mods    []Mod   `json:"mods"`
 }
 
 // List mods installed in the factorio/mods directory
 func listInstalledMods(modDir string) ([]Mod, error) {
-	result := []Mod{}
-
-    file, err := ioutil.ReadFile(modDir + "mod-list.json")
+    file, err := ioutil.ReadFile(modDir + "/mod-list.json")
 
     if err != nil {
         log.Println(err.Error())
         os.Exit(1)
     }
 
+    log.Print(file)
+
+    var result ModsList
     err_json := json.Unmarshal(file, &result)
 
     if err_json != nil {
-        log.Println(err.Error())
+        log.Println(err_json.Error())
         os.Exit(1)
     }
 
     log.Printf("%v", result)
 
-
-	//files, err := ioutil.ReadDir(modDir)
-	//if err != nil {
-	//	log.Printf("Error listing installed mods: %s", err)
-	//	return result, err
-	//}
-	//for _, f := range files {
-	//	if f.Name() == "mod-list.json" {
-	//		continue
-	//	}
-	//	result = append(result, f.Name())
-	//}
-
-	return result, nil
+	return result.Mods, nil
 }
