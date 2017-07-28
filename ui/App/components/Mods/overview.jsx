9c3c7b0d2c415aca6f81315ca5fb1361d9954e86
knoxfighter <philipp@asdff45.de>
2017-07-28 20:08:08 +0200

    fixed missing return statement

--- ui/App/components/Mods/overview.jsx
+++ ui/App/components/Mods/overview.jsx
@@ -3,36 +3,38 @@ import Mod from './Mod';
 
 class ModOverview extends React.Component {
     render() {
-        <div className="box">
-            <div className="box-header">
-                <h3 className="box-title">Manage Mods</h3>
-            </div>
+        return(
+            <div className="box">
+                <div className="box-header">
+                    <h3 className="box-title">Manage Mods</h3>
+                </div>
 
-            <div className="box-body">
-                <div className="table-responsive">
-                    <table className="table table-striped">
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Status</th>
-                            <th>Toggle Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.props.installedMods.map ( (mod, i) => {
-                            return(
-                                <Mod
-                                    key={i}
-                                    mod={mod}
-                                    {...this.props}
-                                />
-                            )
-                        })}
-                        </tbody>
-                    </table>
+                <div className="box-body">
+                    <div className="table-responsive">
+                        <table className="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Status</th>
+                                <th>Toggle Status</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {this.props.installedMods.map ( (mod, i) => {
+                                return(
+                                    <Mod
+                                        key={i}
+                                        mod={mod}
+                                        {...this.props}
+                                    />
+                                )
+                            })}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
+        );
     }
 }
 
