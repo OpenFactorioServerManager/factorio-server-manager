62c469e801508219b94a4cf99a66b569389f56b7
knoxfighter <philipp@asdff45.de>
2018-08-20 02:33:36 +0200

    made js build with webpack only (scss, ... still missing)

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './ui/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    resolve: {
+        alias: {
+            Utilities: path.resolve(__dirname, 'ui/js/')
+        },
+        extensions: ['.wasm', '.mjs', '.js', '.json', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
