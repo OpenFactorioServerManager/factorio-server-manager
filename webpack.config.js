7b8311e151eaa7e4cbc5ad660fd35303dcca8259
knoxfighter <philipp@asdff45.de>
2018-08-29 19:17:11 +0200

    updated to babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,17 @@ module.exports = (env, argv) => {
                     test: /\.jsx?$/,
                     exclude: /node_modules/,
                     use: {
-                        loader: 'babel-loader'
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                [
+                                    '@babel/preset-react', {
+                                        development: !isProduction
+                                    }
+                                ]
+                            ]
+                        }
                     }
                 },
                 {
