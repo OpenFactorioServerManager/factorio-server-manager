07123eb03a3495f99089e60787ee5f6934934df0
Jan Naahs <jan.naahs@naahstea.de>
2020-06-18 01:26:14 +0200

    refactor Login.jsx

--- ui/App/views/Login.jsx
+++ ui/App/views/Login.jsx
@@ -3,6 +3,7 @@ import {useForm} from "react-hook-form";
 import user from "../../api/resources/user";
 import Button from "../elements/Button";
 import {useHistory, useLocation} from "react-router";
+import Panel from "../elements/Panel";
 
 const Login = ({handleLogin}) => {
     const {register, handleSubmit, errors} = useForm();
@@ -29,38 +30,38 @@ const Login = ({handleLogin}) => {
 
     return (
         <div className="h-screen overflow-hidden flex items-center justify-center bg-banner">
-            <form onSubmit={handleSubmit(onSubmit)} className="rounded-sm bg-gray-dark shadow-xl">
-                <div className="px-4 py-2 text-xl text-dirty-white font-bold">
-                    Login
-                </div>
-                <div className="rounded-sm bg-gray-medium shadow-inner mx-4 px-6 pt-4 pb-6 mb-4 flex flex-col">
-                    <div className="mb-4">
-                        <label className="block text-white text-sm font-bold mb-2" htmlFor="username">
-                            Username
-                        </label>
-                        <input className="shadow appearance-none border w-full py-2 px-3 text-black"
-                               ref={register({required: true})}
-                               id="username"
-                               name="username"
-                               type="text" placeholder="Username"/>
-                        {errors.password && <span className="block text-red">Username is required</span>}
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-white text-sm font-bold mb-2" htmlFor="password">
-                            Password
-                        </label>
-                        <input
-                            className="shadow appearance-none w-full py-2 px-3 text-black"
-                            ref={register({required: true})}
-                            name="password"
-                            id="password" type="password" placeholder="******************"/>
-                        {errors.password && <span className="block text-red">Password is required</span>}
-                    </div>
-                    <div className="text-center">
-                        <Button type="success" className="w-full" isSubmit={true}>Sign In</Button>
-                    </div>
-                </div>
-            </form>
+            <Panel
+                title="Login"
+                content={
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="mb-4">
+                            <label className="block text-white text-sm font-bold mb-2" htmlFor="username">
+                                Username
+                            </label>
+                            <input className="shadow appearance-none border w-full py-2 px-3 text-black"
+                                   ref={register({required: true})}
+                                   id="username"
+                                   name="username"
+                                   type="text" placeholder="Username"/>
+                            {errors.password && <span className="block text-red">Username is required</span>}
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-white text-sm font-bold mb-2" htmlFor="password">
+                                Password
+                            </label>
+                            <input
+                                className="shadow appearance-none w-full py-2 px-3 text-black"
+                                ref={register({required: true})}
+                                name="password"
+                                id="password" type="password" placeholder="******************"/>
+                            {errors.password && <span className="block text-red">Password is required</span>}
+                        </div>
+                        <div className="text-center">
+                            <Button type="success" className="w-full" isSubmit={true}>Sign In</Button>
+                        </div>
+                    </form>
+                }
+            />
         </div>
     );
 };
