f34c630ce18341675a75dc34fa1d63577fbab6b2
Jan Naahs <jan.naahs@naahstea.de>
2020-10-24 14:15:33 +0200

    disable Button while loading

--- ui/App/components/Button.jsx
+++ ui/App/components/Button.jsx
@@ -9,13 +9,13 @@ const Button = ({ children, type, onClick, isSubmit, className, size, isLoading,
 
     switch (type) {
         case 'success':
-            color = `bg-green ${isDisabled ? null : "hover:glow-green hover:bg-green-light" }`;
+            color = `bg-green ${isDisabled || isLoading ? null : "hover:glow-green hover:bg-green-light" }`;
             break;
         case 'danger':
-            color = `bg-red ${isDisabled ? null : "hover:glow-red hover:bg-red-light"}`;
+            color = `bg-red ${isDisabled || isLoading ? null : "hover:glow-red hover:bg-red-light"}`;
             break;
         default:
-            color = `bg-gray-light ${isDisabled ? null : "hover:glow-orange hover:bg-orange"}`
+            color = `bg-gray-light ${isDisabled || isLoading ? null : "hover:glow-orange hover:bg-orange"}`
     }
 
     switch (size) {
@@ -27,7 +27,7 @@ const Button = ({ children, type, onClick, isSubmit, className, size, isLoading,
     }
 
     return (
-        <button onClick={onClick} disabled={isDisabled} className={`${className ? className: null} ${isDisabled ? "bg-opacity-50 cursor-not-allowed" : null} ${padding} ${color} inline-block accentuated text-black font-bold`}
+        <button onClick={onClick} disabled={isDisabled || isLoading} className={`${className ? className: null} ${isDisabled || isLoading ? "bg-opacity-50 cursor-not-allowed" : null} ${padding} ${color} inline-block accentuated text-black font-bold`}
         type={isSubmit ? 'submit' : 'button'}>
             {children} { isLoading && <FontAwesomeIcon icon={faSpinner} spin={true}/>}
         </button>
