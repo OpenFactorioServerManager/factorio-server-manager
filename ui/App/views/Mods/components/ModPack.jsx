6c2dff47cc1a4a736e57b8e51bcbf1cc77261438
Jan Naahs <jan.naahs@naahstea.de>
2020-11-27 19:45:30 +0100

    load modpack requires confirmation including a warning text

--- ui/App/views/Mods/components/ModPack.jsx
+++ ui/App/views/Mods/components/ModPack.jsx
@@ -3,10 +3,13 @@ import {faTrashAlt, faSpinner, faUpload} from "@fortawesome/free-solid-svg-icons
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import modsResource from "../../../../api/resources/mods";
 import ModList from "./ModList";
+import ConfirmDialog from "../../../components/ConfirmDialog";
 
 const ModPack = ({modPack, reloadModPacks, factorioVersion, reloadMods}) => {
 
     const [isLoading, setIsLoading] = useState(false);
+    const [isLoadModPackDialogOpen, setIsLoadModPackDialogOpen] = useState(false);
+
 
     const deleteModPack = modName => {
         return modsResource.packs
@@ -40,7 +43,7 @@ const ModPack = ({modPack, reloadModPacks, factorioVersion, reloadMods}) => {
 
     const loadModPack = name => {
         setIsLoading(true)
-        modsResource.packs
+        return modsResource.packs
             .load(name)
             .then(reloadMods)
             .finally(() => setIsLoading(false))
@@ -52,7 +55,15 @@ const ModPack = ({modPack, reloadModPacks, factorioVersion, reloadMods}) => {
                 <h2 className="text-lg text-dirty-white mb-1 inline">{modPack.name}</h2>
                 <div className="flex space-x-2">
                     <FontAwesomeIcon className="text-blue cursor-pointer hover:text-blue-light inline"
-                                     onClick={() => loadModPack(modPack.name)} spin={isLoading} icon={isLoading ? faSpinner : faUpload}/>
+                                     onClick={() => setIsLoadModPackDialogOpen(true)} spin={isLoading} icon={isLoading ? faSpinner : faUpload}/>
+                    <ConfirmDialog
+                        title="Load ModPack"
+                        content={`Loading the ModPack ${modPack.name} will remove all installed Mods.`}
+                        isOpen={isLoadModPackDialogOpen}
+                        close={() => setIsLoadModPackDialogOpen(false)}
+                        onSuccess={() => loadModPack(modPack.name)}
+                    />
+
                     <FontAwesomeIcon className="text-red cursor-pointer hover:text-red-light inline"
                                      onClick={() => deleteModPack(modPack.name)} icon={faTrashAlt}/>
                 </div>
