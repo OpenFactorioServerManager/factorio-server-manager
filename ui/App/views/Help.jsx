a499cea1234809b83a51fbf75c622bbdb8851a1c
Mitch Roote <mitch@r00t.ca>
2020-12-30 16:35:42 -0500

    Update help page URL

--- ui/App/views/Help.jsx
+++ ui/App/views/Help.jsx
@@ -11,7 +11,7 @@ const Help = () => {
                     <p className="mb-2">The Factorio Serve Manager (FSM) is an open source project and is not affiliated to the game Factorio or Wube Software.</p>
 
                     <h2 className="text-dirty-white">Bugs and Help</h2>
-                    <p className="mb-4">Please use the <a className="text-blue hover:text-blue-light" target="_blank" href="https://github.com/mroote/factorio-server-manager/issues">GitHub repository</a> to report bugs or seek for help.</p>
+                    <p className="mb-4">Please use the <a className="text-blue hover:text-blue-light" target="_blank" href="https://github.com/OpenFactorioServerManager/factorio-server-manager/issues">GitHub repository</a> to report bugs or seek for help.</p>
 
                     <h1 className="mb-1 text-xl text-dirty-white">Helpful Resources</h1>
                     <p className="mb-2"><a className="text-blue hover:text-blue-light" target="_blank" href="https://wiki.factorio.com/Multiplayer">Official Factorio Wiki about Multiplayer</a></p>
