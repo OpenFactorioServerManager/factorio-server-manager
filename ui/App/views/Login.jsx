ff3cad0bb718964044f1a051f014d865e1e4e987
knoxfighter <knoxfighter@asdff45.de>
2021-01-30 17:50:09 +0100

    fix redirect after login failed #247

--- ui/App/views/Login.jsx
+++ ui/App/views/Login.jsx
@@ -22,6 +22,7 @@ const Login = ({handleLogin}) => {
                 history.push('/');
             }
         } catch (e) {
+            console.log(e);
             window.flash("Login failed. Username or Password wrong.", "red");
         }
     };
