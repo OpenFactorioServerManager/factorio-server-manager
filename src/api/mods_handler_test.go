88d09d524745f48f0256a0754302bf683953c1e8
knoxfighter <knoxfighter@asdff45.de>
2023-11-02 17:04:49 +0100

    Update go dependencies (#370)
    * updated go dependencies
    * fixed config always loading program flags
    * required go version is 1.18 now
    * don't fail when env wasn't loaded in tests
    * fixed conf/env for tests

--- src/api/mods_handler_test.go
+++ src/api/mods_handler_test.go
@@ -23,13 +23,16 @@ import (
 func TestMain(m *testing.M) {
 	var err error
 
-	godotenv.Load("../.env")
+	err = godotenv.Load("../.env")
+	if err != nil {
+		log.Println("WARN: wasn't able to load env: ", err)
+	}
 
 	// basic setup stuff
 	bootstrap.NewConfig([]string{
-		"--dir", os.Getenv("dir"),
-		"--conf", os.Getenv("conf"),
-		"--mod-pack-dir", os.Getenv("mod_pack_dir"),
+		"--dir", os.Getenv("FSM_DIR"),
+		"--conf", os.Getenv("FSM_CONF"),
+		"--mod-pack-dir", os.Getenv("FSM_MODPACK_DIR"),
 		"--mod-dir", os.Getenv("mod_dir"),
 	})
 
