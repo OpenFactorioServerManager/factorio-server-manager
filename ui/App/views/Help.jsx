fad1051c22f53ecfacf4fde6917a93fc6890f6a4
= <=>
2023-11-06 14:08:01 +0100

    Fixed Typo in Help.jsx

--- ui/App/views/Help.jsx
+++ ui/App/views/Help.jsx
@@ -8,7 +8,7 @@ const Help = () => {
             content={
                 <>
                     <h1 className="text-xl text-dirty-white">Factorio Server Manager</h1>
-                    <p className="mb-2">The Factorio Serve Manager (FSM) is an open source project and is not affiliated to the game Factorio or Wube Software.</p>
+                    <p className="mb-2">The Factorio Server Manager (FSM) is an open source project and is not affiliated to the game Factorio or Wube Software.</p>
 
                     <h2 className="text-dirty-white">Bugs and Help</h2>
                     <p className="mb-4">Please use the <a className="text-blue hover:text-blue-light" target="_blank" href="https://github.com/OpenFactorioServerManager/factorio-server-manager/issues">GitHub repository</a> to report bugs or seek for help.</p>
